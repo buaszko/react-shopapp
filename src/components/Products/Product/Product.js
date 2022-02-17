826d65170cbc3764ef9bb05489f6dab01aa7f2c7
buaszko <blazej.nagiecki@gmail.com>
2022-02-17 18:57:23 +0100

    new begin and works fine

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -1,27 +1,27 @@
 import styles from './Product.module.scss';
 import PropTypes from 'prop-types'
 import { useState , useMemo} from 'react';
-import ProductImage from '../Products/Product/ProductImage/ProductImage';
-import ProductForm from '../Products/Product/ProductForm/ProductForm'
+import ProductImage from './ProductImage/ProductImage';
+import ProductForm from './ProductForm/ProductForm';
 
 const Product = props => {
-
-  const [currentColor, setCurrentColor] = useState(props.colors[0])
-  const [currentSize, setCurrentSize] = useState(props.sizes[0].name)
+  const data = props.data;
+  const [currentColor, setCurrentColor] = useState(data.colors[0])
+  const [currentSize, setCurrentSize] = useState(data.sizes[0].name)
   const prepareColorClassName = color => {
   return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()] 
   };
 
   const getPrice = useMemo(() =>{
-    const suma = props.sizes.find( ({ name }) => name === currentSize)
-    return props.basePrice + suma.additionalPrice
-  }, [props.sizes, props.basePrice, currentSize]);
-
+    const suma = data.sizes.find( ({ name }) => name === currentSize)
+    return data.basePrice + suma.additionalPrice
+  }, [data.sizes, data.basePrice, currentSize]);
+ 
   const hundleSubmit = (e) => {
     e.preventDefault()
     console.log('Summary');
     console.log('========');
-    console.log('Name:', props.title);
+    console.log('Name:', data.title);
     console.log('Price:', getPrice);
     console.log('Size:', currentSize);
     console.log('Color:', currentColor);
@@ -29,15 +29,14 @@ const Product = props => {
 
   return (
     <article className={styles.product}>
-       <ProductImage title={props.title} name={props.name} currentColor={currentColor}/>
+       <ProductImage title={data.title} name={data.name} currentColor={currentColor}/>
       <div>
         <header>
-          <h2 className={styles.name}>{props.title}</h2>
+          <h2 className={styles.name}>{data.title}</h2>
           <span className={styles.price}>Price: {getPrice}$</span>
         </header>
        <ProductForm 
-        sizes={props.sizes}
-        colors={props.colors}
+        data={data}
         hundleSubmit={hundleSubmit}
         currentSize={currentSize}
         setCurrentSize={setCurrentSize}
@@ -52,10 +51,7 @@ const Product = props => {
 };
 
 Product.propTypes = {
-  id:PropTypes.number.isRequired,
-  title:PropTypes.string.isRequired,
-  basePrice:PropTypes.number.isRequired,
-  name:PropTypes.string.isRequired,
-};
+  data:PropTypes.object
 
+};
 export default Product;
